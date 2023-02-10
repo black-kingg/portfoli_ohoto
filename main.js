ddaa163b0ddc6174b6b8ce5a6cf2cfdd13cf2c53
Odetunde Mubarak <93845490+blackingg@users.noreply.github.com>
2023-02-10 20:08:46 +0100

    e

--- main.js
+++ main.js
@@ -19,3 +19,27 @@ textButtons.forEach(textButton =>{
 
     text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style= "transform: rotate(${index * 12}deg)">${character}</span>`).join('')
 })
+
+const nav = document.querySelector('.nav_links');
+const openNavBtn = document.querySelector('#nav_toggle-open');
+const closeNavBtn = document.querySelector('#nav_toggle-close');
+
+const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none'
+    closeNavBtn.style.display = 'inline-block'
+}
+
+openNavBtn.addEventListener('click', openNav)
+
+const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block'
+    closeNavBtn.style.display = 'none'
+}
+
+closeNavBtn.addEventListener('click', closeNav)
+
+nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav)
+})
